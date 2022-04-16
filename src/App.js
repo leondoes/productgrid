42f9991bb381f43175b42faebb58bb6ac2e61e82
leondoes <50689177+leondoes@users.noreply.github.com>
2022-04-16 14:48:46 -0400

    🚑  Hotfix font import old style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+import "./styles.css";
 
 import ProductGrid from "/src/components/ProductGrid";
-
-import RobotoWoff2 from "/public/fonts/roboto-v29-latin-regular.woff2";
-import RobotoWoff from "/public/fonts/roboto-v29-latin-regular.woff";
-
-const GlobalFonts = createGlobalStyle`
-@font-face {
-        font-family: 'Roboto';
-        src: local('Roboto'), local('Roboto'),
-        url(${RobotoWoff2}) format('woff2'),
-        url(${RobotoWoff}) format('woff');
-        font-weight: 300;
-        font-style: normal;
-    }
-`;
 
 const AppContainer = styled.div`
   font-family: Roboto;
@@ -23,11 +10,8 @@ const AppContainer = styled.div`
 
 export default function App() {
   return (
-    <div>
-      <GlobalFonts />
-      <AppContainer>
-        <ProductGrid />
-      </AppContainer>
-    </div>
+    <AppContainer>
+      <ProductGrid />
+    </AppContainer>
   );
 }
