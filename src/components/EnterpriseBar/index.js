60d83e1d795f7f12d6b387b2cb601254eb0022ea
leondoes <50689177+leondoes@users.noreply.github.com>
2022-07-05 02:18:48 -0400

    ✨ Added more content to enterprise bar

--- src/components/EnterpriseBar/index.js
+++ src/components/EnterpriseBar/index.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { StyledBar, StyledImg, StyledText, StyledDivider } from "./styled";
+import {
+  BarContainer,
+  CanadasStoreImg,
+  BarText,
+  BarDivider,
+  LeftSideBar,
+  RightSideBar
+} from "./styled";
 import img from "/src/assets/PencilBanner.webp";
 
 const EnterpriseBar = () => (
-  <StyledBar>
-    <StyledImg src={img} alt="Pencil Banner" />
-    <StyledText>Made for life in Canada</StyledText>
-    <StyledDivider />
-    <StyledText>Credit offered*</StyledText>
-  </StyledBar>
+  <BarContainer>
+    <CanadasStoreImg src={img} alt="Pencil Banner" />
+    <LeftSideBar>
+      <BarText>Made for life in Canada</BarText>
+      <BarDivider />
+      <BarText>Credit offered*</BarText>
+    </LeftSideBar>
+    <RightSideBar>
+      <BarText>Account</BarText>
+      <BarText>Support</BarText>
+      <BarDivider />
+      <BarText>FR</BarText>
+    </RightSideBar>
+  </BarContainer>
 );
 
 export default EnterpriseBar;
--- src/components/EnterpriseBar/styled.js
+++ src/components/EnterpriseBar/styled.js
@@ -1,27 +1,38 @@
 import styled from "styled-components";
 
-export const StyledBar = styled.div`
+export const BarContainer = styled.div`
   display: flex;
   background-color: #313131;
   height: 40px;
   align-items: center;
+  justify-content: space-between;
 `;
 
-export const StyledImg = styled.img`
-  align-self: center;
+export const CanadasStoreImg = styled.img`
   margin-right: 10px;
 `;
 
-export const StyledText = styled.div`
+export const BarText = styled.div`
   color: #fff;
   text-align: left;
   font-size: 12px;
   line-height: 20px;
   letter-spacing: 0;
   font-family: "Lato", sans-serif;
+  margin: 0px 5px;
 `;
 
-export const StyledDivider = styled.div`
+export const LeftSideBar = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
+export const RightSideBar = styled.div`
+  display: flex;
+  text-align: right;
+  margin-right: 10px;
+`;
+
+export const BarDivider = styled.div`
   width: 0.0625rem;
   height: 1rem;
   background: #525252 0 0 no-repeat padding-box;
