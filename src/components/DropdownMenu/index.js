b46cefe3ba74a16fed578988488d4834869a9a6e
leondoes <50689177+leondoes@users.noreply.github.com>
2022-08-03 01:27:57 -0400

    ✨ Clicking outside the account menu hides it

--- src/components/DropdownMenu/index.js
+++ src/components/DropdownMenu/index.js
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
-import {
-  AccountMenuText,
-  AccountButton,
-  MenuContainer,
-  DropdownContainer,
-  Top,
-  Middle,
-  Bottom,
-  StyledLink,
-  MenuLink,
-  TriangleRewards
-} from "./styled";
+import React, { useState, useRef, useEffect } from "react";
+import { AccountMenuText, AccountButton, MenuContainer } from "./styled";
+import { DropdownContainer, Top, Middle, Bottom, StyledLink } from "./styled";
+import { MenuLink, TriangleRewards } from "./styled";
 import img from "/src/assets/TriangleRewards.webp";
 
 const DropdownMenu = () => {
   const [displayed, setDisplayed] = useState(false);
+  const dropdownContainerRef = useRef(null);
   const handleOnClick = () => setDisplayed((prevDisplayed) => !prevDisplayed);
+  const HideDropdownMenu = (ref) => {
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+          handleOnClick();
+        }
+      };
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  };
 
+  HideDropdownMenu(dropdownContainerRef, handleOnClick);
   return (
     <MenuContainer>
       <AccountButton onClick={handleOnClick}>
@@ -24,7 +32,7 @@ const DropdownMenu = () => {
       </AccountButton>
 
       {displayed && (
-        <DropdownContainer>
+        <DropdownContainer ref={dropdownContainerRef}>
           <Top>
             <StyledLink to="/">Sign in</StyledLink> or{" "}
             <StyledLink to="/">Register</StyledLink>
